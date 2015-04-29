05a6edc1f12d7724d85ba9a91176e2d0696b52c9
Charles Sprayberry <charles@designbook.com>
2015-04-29 19:25:37 -0400

    get rid of templates

--- src/Labrador/Http/Controller/WelcomeController.php
+++ src/Labrador/Http/Controller/WelcomeController.php
@@ -11,12 +11,35 @@ namespace Labrador\Http\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 
-class WelcomeController extends RenderingController {
+class WelcomeController {
 
     public function index() {
-        $this->set('css', ['https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css']);
-        $content = $this->render('welcome/index');
-        return new Response($content);
+        return new Response($this->getHtml());
+    }
+
+    private function getHtml() {
+        return <<<HTML
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Labrador HTTP Application</title>
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" rel="stylesheet" />
+    </head>
+    <body>
+        <header>
+            <h1>Labrador HTTP Application</h1>
+        </header>
+        <section id="main">
+            <p>Welcome to <a href="https://github.com/cspray/labrador-http">Labrador HTTP</a>, a microframework written on top of <a href="">Labrador</a>.</p>
+        </section>
+        <footer>
+
+        </footer>
+    </body>
+</html>
+
+HTML;
+
     }
 
 }
