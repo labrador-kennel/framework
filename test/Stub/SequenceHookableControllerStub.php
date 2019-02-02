90ec43e91a8536404d7c9a744106fcde9143c167
Charles <cspray@users.noreply.github.com>
2019-02-02 15:49:39 -0500

    Add a HookableController to short-circuit normal handling or decorate a response (#16)

---
+++ test/Stub/SequenceHookableControllerStub.php
@@ -0,0 +1,38 @@
+<?php declare(strict_types=1);
+
+namespace Cspray\Labrador\Http\Test\Stub;
+
+use Amp\Http\Server\Request;
+use Amp\Http\Server\Response;
+use Amp\Promise;
+use Amp\Success;
+use Cspray\Labrador\Http\Controller\HookableController;
+
+class SequenceHookableControllerStub extends HookableController {
+
+    private $requests = [];
+
+    public function beforeAction(Request $request): Promise {
+        $this->requests[] = ['beforeAction', $request];
+        return new Success();
+    }
+
+    protected function handle(Request $request) : Promise {
+        $this->requests[] = ['handle', $request];
+        return new Success(new Response());
+    }
+
+    public function afterAction(Request $request, Response $response): Promise {
+        $this->requests[] = ['afterAction', $request];
+        return new Success();
+    }
+
+    public function getReceivedRequests() : array {
+        return $this->requests;
+    }
+
+    public function toString() : string {
+        return self::class;
+    }
+
+}
