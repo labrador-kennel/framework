65714640427f7e57462587cb91da598a5fbf32be
Charles Sprayberry <cspray@users.noreply.github.com>
2023-03-06 10:10:50 -0500

    Improve versions output in bootstrap (#42)

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -5,6 +5,7 @@ namespace Labrador\Http;
 use Amp\Http\Server\ErrorHandler;
 use Cspray\AnnotatedContainer\AnnotatedContainerVersion;
 use Cspray\AnnotatedContainer\Bootstrap\Bootstrap as AnnotatedContainerBootstrap;
+use PackageVersions\Versions;
 use function Amp\ByteStream\getStdout;
 
 final class Bootstrap {
@@ -19,9 +20,10 @@ final class Bootstrap {
 
     public function bootstrapApplication() : BootstrapResults {
         getStdout()->write(file_get_contents(__DIR__ . '/../resources/ascii/labrador.txt') . PHP_EOL);
-        getStdout()->write('Labrador HTTP Version: dev-main' . PHP_EOL);
-        getStdout()->write('Annotated Container Version: ' . AnnotatedContainerVersion::getVersion() . PHP_EOL);
-        getStdout()->write('Amp HTTP Server Version: v3.0.0' . PHP_EOL . PHP_EOL);
+        getStdout()->write('PHP Version: ' . PHP_VERSION);
+        getStdout()->write('Labrador HTTP Version: ' . Versions::getVersion('labrador-kennel/http') . PHP_EOL);
+        getStdout()->write('Annotated Container Version: ' . Versions::getVersion('cspray/annotated-container') . PHP_EOL);
+        getStdout()->write('Amp HTTP Server Version: ' . Versions::getVersion('amphp/http-server') . PHP_EOL . PHP_EOL);
 
         $container = $this->bootstrap->bootstrapContainer(profiles: $this->profiles);
 
