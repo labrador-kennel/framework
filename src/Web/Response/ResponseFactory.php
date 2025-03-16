a7e36cbced111547da5fd31be3aa6444f3575fc2
Charles Sprayberry <cspray@users.noreply.github.com>
2025-03-16 09:10:36 -0400

    Introduce start of ResponseFactory (#79)
    * Introduce start of ResponseFactory
    
    * Fix styling issues

---
+++ src/Web/Response/ResponseFactory.php
@@ -0,0 +1,37 @@
+<?php declare(strict_types=1);
+
+namespace Labrador\Web\Response;
+
+use Amp\Http\HttpMessage;
+use Amp\Http\HttpStatus;
+use Amp\Http\Server\Response;
+use Cspray\AnnotatedContainer\Attribute\Service;
+use Labrador\Template\RenderedTemplate;
+
+/**
+ * @psalm-import-type HeaderParamArrayType from HttpMessage
+ */
+#[Service]
+final class ResponseFactory {
+
+    /**
+     * @param string|RenderedTemplate $content
+     * @param HeaderParamArrayType $headers
+     * @param int $status
+     * @return Response
+     */
+    public function html(
+        string|RenderedTemplate $content,
+        array $headers = [],
+        int $status = HttpStatus::OK
+    ) : Response {
+        $body = $content instanceof RenderedTemplate ? $content->toString() : $content;
+        $headers = array_merge([], ['Content-Type' => 'text/html; charset=utf-8'], $headers);
+
+        return new Response(
+            status: $status,
+            headers: $headers,
+            body: $body
+        );
+    }
+}
