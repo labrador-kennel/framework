abe6577e37778ae14f6485ddd9078e05de9c8eeb
Charles Sprayberry <me@cspray.io>
2025-03-16 09:07:57 -0400

    Fix styling issues

--- src/Web/Response/ResponseFactory.php
+++ src/Web/Response/ResponseFactory.php
@@ -34,5 +34,4 @@ final class ResponseFactory {
             body: $body
         );
     }
-
 }
--- test/Unit/Web/Response/ResponseFactoryTest.php
+++ test/Unit/Web/Response/ResponseFactoryTest.php
@@ -82,5 +82,4 @@ final class ResponseFactoryTest extends TestCase {
             $response->getStatus()
         );
     }
-
 }
