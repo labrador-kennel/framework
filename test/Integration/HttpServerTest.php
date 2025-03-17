520a4dba9382d18837a9650fbb2a8aaba3943a0f
Charles Sprayberry <me@cspray.io>
2025-03-17 16:18:04 -0400

    Check if HttpServerTest is causing segfault in CI

--- test/Integration/HttpServerTest.php
+++ test/Integration/HttpServerTest.php
@@ -69,92 +69,92 @@ class HttpServerTest extends AsyncTestCase {
         StreamBuffer::reset(self::$stderr);
     }
 
-    public function testMakingHelloWorldCall() : void {
-        $client = (new HttpClientBuilder())->build();
-
-        $response = $client->request(new Request('http://localhost:4200/hello/world'));
-
-        self::assertSame(HttpStatus::OK, $response->getStatus());
-        self::assertSame('Hello, world!', $response->getBody()->buffer());
-    }
-
-    public function testGlobalMiddlewareAddedFromAppRouteListenerIsInvoked() : void {
-        $client = (new HttpClientBuilder())->build();
-
-        $response = $client->request(new Request('http://localhost:4200/hello/world'));
-
-        self::assertSame(HttpStatus::OK, $response->getStatus());
-
-        /** @var MiddlewareCallRegistry $callRegistry */
-        $callRegistry = self::$container->get(MiddlewareCallRegistry::class);
-
-        $expected = [
-            QuxMiddleware::class,
-            FooMiddleware::class,
-            BazMiddleware::class,
-            BarMiddleware::class
-        ];
-
-        self::assertSame($expected, $callRegistry->getCalled());
-    }
-
-    public function testRouteMiddlewareRespected() : void {
-        $client = (new HttpClientBuilder())->build();
-
-        $request = new Request('http://localhost:4200/hello/middleware', 'GET');
-        $response = $client->request($request);
-
-        self::assertSame(HttpStatus::OK, $response->getStatus());
-        self::assertSame('Hello, Universe!', $response->getBody()->buffer());
-    }
-
-    public function methodDataProvider() : array {
-        return [
-            ['GET'],
-            ['POST'],
-            ['PUT'],
-            ['DELETE']
-        ];
-    }
-
-    public function testCorrectAccessLogOutputSendToStdout() : void {
-        $client = (new HttpClientBuilder())->build();
-
-        $request = new Request('http://localhost:4200/hello/world');
-        $client->request($request);
-
-        $handler = self::$container->get(DummyLoggerFactory::class)->testHandler;
-        self::assertInstanceOf(TestHandler::class, $handler);
-
-        self::assertTrue($handler->hasInfoThatPasses(static function (LogRecord $record) {
-            $expected = <<<TEXT
-%a labrador.app.INFO: "GET http://localhost:%d/hello/world" 200 "OK" HTTP/1.1 127.0.0.1:%d on 127.0.0.1:%d {"request":{"method":"GET","uri":"http://localhost:4200/hello/world","protocolVersion":"1.1","local":"127.0.0.1:%d","remote":"127.0.0.1:%d"},"response":{"status":200,"reason":"OK"}} []
-TEXT;
-            return (new StringMatchesFormatDescription($expected))->evaluate(
-                other: $record->formatted,
-                returnResult: true
-            );
-        }));
-    }
-
-    public function testExceptionThrowHasCorrectLogOutputSentToStdout() : void {
-        $client = (new HttpClientBuilder())->build();
-
-        $request = new Request('http://localhost:4200/exception');
-        $client->request($request);
-
-        $handler = self::$container->get(DummyLoggerFactory::class)->testHandler;
-        self::assertInstanceOf(TestHandler::class, $handler);
-
-        self::assertTrue($handler->hasErrorThatPasses(static function (LogRecord $record) {
-            $expectedContext = '{"client_address":"127.0.0.1:%d","method":"GET","path":"/exception","exception_class":"RuntimeException","file":"%a/RouterListener.php","line_number":47,"exception_message":"A message detailing what went wrong that should show up in logs.","stack_trace":%a}';
-            $expected = <<<TEXT
-%a labrador.app.ERROR: RuntimeException thrown in %a/RouterListener.php#L47 handling client 127.0.0.1:%d with request "GET /exception". Message: A message detailing what went wrong that should show up in logs. $expectedContext []
-TEXT;
-            return (new StringMatchesFormatDescription($expected))->evaluate(
-                other: $record->formatted,
-                returnResult: true
-            );
-        }));
-    }
+//    public function testMakingHelloWorldCall() : void {
+//        $client = (new HttpClientBuilder())->build();
+//
+//        $response = $client->request(new Request('http://localhost:4200/hello/world'));
+//
+//        self::assertSame(HttpStatus::OK, $response->getStatus());
+//        self::assertSame('Hello, world!', $response->getBody()->buffer());
+//    }
+//
+//    public function testGlobalMiddlewareAddedFromAppRouteListenerIsInvoked() : void {
+//        $client = (new HttpClientBuilder())->build();
+//
+//        $response = $client->request(new Request('http://localhost:4200/hello/world'));
+//
+//        self::assertSame(HttpStatus::OK, $response->getStatus());
+//
+//        /** @var MiddlewareCallRegistry $callRegistry */
+//        $callRegistry = self::$container->get(MiddlewareCallRegistry::class);
+//
+//        $expected = [
+//            QuxMiddleware::class,
+//            FooMiddleware::class,
+//            BazMiddleware::class,
+//            BarMiddleware::class
+//        ];
+//
+//        self::assertSame($expected, $callRegistry->getCalled());
+//    }
+//
+//    public function testRouteMiddlewareRespected() : void {
+//        $client = (new HttpClientBuilder())->build();
+//
+//        $request = new Request('http://localhost:4200/hello/middleware', 'GET');
+//        $response = $client->request($request);
+//
+//        self::assertSame(HttpStatus::OK, $response->getStatus());
+//        self::assertSame('Hello, Universe!', $response->getBody()->buffer());
+//    }
+//
+//    public function methodDataProvider() : array {
+//        return [
+//            ['GET'],
+//            ['POST'],
+//            ['PUT'],
+//            ['DELETE']
+//        ];
+//    }
+//
+//    public function testCorrectAccessLogOutputSendToStdout() : void {
+//        $client = (new HttpClientBuilder())->build();
+//
+//        $request = new Request('http://localhost:4200/hello/world');
+//        $client->request($request);
+//
+//        $handler = self::$container->get(DummyLoggerFactory::class)->testHandler;
+//        self::assertInstanceOf(TestHandler::class, $handler);
+//
+//        self::assertTrue($handler->hasInfoThatPasses(static function (LogRecord $record) {
+//            $expected = <<<TEXT
+//%a labrador.app.INFO: "GET http://localhost:%d/hello/world" 200 "OK" HTTP/1.1 127.0.0.1:%d on 127.0.0.1:%d {"request":{"method":"GET","uri":"http://localhost:4200/hello/world","protocolVersion":"1.1","local":"127.0.0.1:%d","remote":"127.0.0.1:%d"},"response":{"status":200,"reason":"OK"}} []
+//TEXT;
+//            return (new StringMatchesFormatDescription($expected))->evaluate(
+//                other: $record->formatted,
+//                returnResult: true
+//            );
+//        }));
+//    }
+//
+//    public function testExceptionThrowHasCorrectLogOutputSentToStdout() : void {
+//        $client = (new HttpClientBuilder())->build();
+//
+//        $request = new Request('http://localhost:4200/exception');
+//        $client->request($request);
+//
+//        $handler = self::$container->get(DummyLoggerFactory::class)->testHandler;
+//        self::assertInstanceOf(TestHandler::class, $handler);
+//
+//        self::assertTrue($handler->hasErrorThatPasses(static function (LogRecord $record) {
+//            $expectedContext = '{"client_address":"127.0.0.1:%d","method":"GET","path":"/exception","exception_class":"RuntimeException","file":"%a/RouterListener.php","line_number":47,"exception_message":"A message detailing what went wrong that should show up in logs.","stack_trace":%a}';
+//            $expected = <<<TEXT
+//%a labrador.app.ERROR: RuntimeException thrown in %a/RouterListener.php#L47 handling client 127.0.0.1:%d with request "GET /exception". Message: A message detailing what went wrong that should show up in logs. $expectedContext []
+//TEXT;
+//            return (new StringMatchesFormatDescription($expected))->evaluate(
+//                other: $record->formatted,
+//                returnResult: true
+//            );
+//        }));
+//    }
 }
