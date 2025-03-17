f9f98e0cfc3e908af3a7c1073fe6e248d7780743
Charles Sprayberry <me@cspray.io>
2025-03-17 18:23:15 -0400

    Introduce controller helper to work with CSRF token

---
+++ src/Web/Session/CsrfTokenHelper.php
@@ -0,0 +1,31 @@
+<?php declare(strict_types=1);
+
+namespace Labrador\Web\Session;
+
+use Amp\Http\Server\Request;
+use Amp\Http\Server\Session\Session;
+use Labrador\Web\Session\Exception\SessionHasNoCsrfToken;
+use Labrador\Web\Session\Exception\SessionNotAttachedToRequest;
+
+final class CsrfTokenHelper {
+
+    public function token(Request $request) : string {
+        if (!$request->hasAttribute(Session::class)) {
+            throw SessionNotAttachedToRequest::fromSessionNotAttachedToRequest();
+        }
+
+        $session = $request->getAttribute(Session::class);
+        assert($session instanceof Session);
+
+        if (!$session->has('labrador.csrfToken')) {
+            throw SessionHasNoCsrfToken::fromSessionDoesNotHaveCsrfToken();
+        }
+
+        return (string) $session->get('labrador.csrfToken');
+    }
+
+    public function isTokenValid(Request $request, string $token) : bool {
+        return $this->token($request) === $token;
+    }
+
+}
