b31f1c887d17eed07b22af301862be775c514b41
Charles Sprayberry <me@cspray.io>
2025-03-17 16:21:38 -0400

    Check if AsyncTestCase causing problems

--- test/Unit/Web/Controller/HookableControllerTest.php
+++ test/Unit/Web/Controller/HookableControllerTest.php
@@ -15,52 +15,52 @@ use League\Uri\Http;
 
 class HookableControllerTest extends AsyncTestCase {
 
-    public function testRequestReceivingCallsInvokedInOrder() {
-        $subject = new SequenceHookableControllerStub();
-        $client = $this->createMock(Client::class);
-        $request = new Request($client, 'GET', Http::createFromString('/'));
-        $subject->handleRequest($request);
-
-        $expected = [
-            ['beforeAction', $request],
-            ['handle', $request],
-            ['afterAction', $request],
-        ];
-        $this->assertSame($expected, $subject->getReceivedRequests());
-    }
-
-    public function testReturningResponseFromBeforeActionShortCircuitsHandle() {
-        $subject = new BeforeActionResponseHookableControllerStub();
-        $client = $this->createMock(Client::class);
-        $request = new Request($client, 'GET', Http::createFromString('/'));
-        $response = $subject->handleRequest($request);
-        $body = $response->getBody()->read();
-
-        $this->assertSame(HttpStatus::OK, $response->getStatus());
-        $this->assertSame('From beforeAction', $body);
-    }
-
-    public function testReturningResponseFromAfterActionOverridesHandle() {
-        $subject = new AfterActionResponseDecoratorHookableControllerStub();
-        $client = $this->createMock(Client::class);
-        $request = new Request($client, 'GET', Http::createFromString('/'));
-        $response = $subject->handleRequest($request);
-        $body = $response->getBody()->read();
-
-        $this->assertSame(HttpStatus::OK, $response->getStatus());
-        $this->assertSame('A-OK', $body);
-    }
-
-    public function testReturnsResponseFromHandleIfNoBeforeOrAfterActionResponse() {
-        $subject = new OnlyHandlerHookableControllerStub();
-        $client = $this->createMock(Client::class);
-        $request = new Request($client, 'GET', Http::createFromString('/'));
-        $response = $subject->handleRequest($request);
-        $this->assertInstanceOf(Response::class, $response);
-
-        $body = $response->getBody()->read();
-
-        $this->assertSame(HttpStatus::OK, $response->getStatus());
-        $this->assertSame('From Only Handler', $body);
-    }
+//    public function testRequestReceivingCallsInvokedInOrder() {
+//        $subject = new SequenceHookableControllerStub();
+//        $client = $this->createMock(Client::class);
+//        $request = new Request($client, 'GET', Http::createFromString('/'));
+//        $subject->handleRequest($request);
+//
+//        $expected = [
+//            ['beforeAction', $request],
+//            ['handle', $request],
+//            ['afterAction', $request],
+//        ];
+//        $this->assertSame($expected, $subject->getReceivedRequests());
+//    }
+//
+//    public function testReturningResponseFromBeforeActionShortCircuitsHandle() {
+//        $subject = new BeforeActionResponseHookableControllerStub();
+//        $client = $this->createMock(Client::class);
+//        $request = new Request($client, 'GET', Http::createFromString('/'));
+//        $response = $subject->handleRequest($request);
+//        $body = $response->getBody()->read();
+//
+//        $this->assertSame(HttpStatus::OK, $response->getStatus());
+//        $this->assertSame('From beforeAction', $body);
+//    }
+//
+//    public function testReturningResponseFromAfterActionOverridesHandle() {
+//        $subject = new AfterActionResponseDecoratorHookableControllerStub();
+//        $client = $this->createMock(Client::class);
+//        $request = new Request($client, 'GET', Http::createFromString('/'));
+//        $response = $subject->handleRequest($request);
+//        $body = $response->getBody()->read();
+//
+//        $this->assertSame(HttpStatus::OK, $response->getStatus());
+//        $this->assertSame('A-OK', $body);
+//    }
+//
+//    public function testReturnsResponseFromHandleIfNoBeforeOrAfterActionResponse() {
+//        $subject = new OnlyHandlerHookableControllerStub();
+//        $client = $this->createMock(Client::class);
+//        $request = new Request($client, 'GET', Http::createFromString('/'));
+//        $response = $subject->handleRequest($request);
+//        $this->assertInstanceOf(Response::class, $response);
+//
+//        $body = $response->getBody()->read();
+//
+//        $this->assertSame(HttpStatus::OK, $response->getStatus());
+//        $this->assertSame('From Only Handler', $body);
+//    }
 }
